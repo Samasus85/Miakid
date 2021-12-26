c60ec217cb29fadd48f368890a70a1a08a95395b
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2021-12-26 17:09:03 +0300

    Update script28.js

--- script/script28.js
+++ script/script28.js
@@ -1,11 +1,4 @@
 let title = document.getElementsByClassName('main-card-title');
-console.log(title[1].innerHTML)
-
 let images = document.getElementsByClassName('main-card-img');
-console.log(images)
 let prices = document.getElementsByClassName('price');
-console.log(prices[2].innerHTML)
 let addBtn = document.getElementsByClassName('add-to-card')
-console.log(addBtn[4].innerHTML)
-
-
