55394d90a12bc3d50d5ce70a87c2aa9a524eb8da
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2021-12-26 17:24:11 +0300

    Update hw33.js

--- script/hw33.js
+++ script/hw33.js
@@ -1,15 +1,9 @@
 let addToCard = document.querySelector('.add-to-card');
-console.log(addToCard)
 let addTo = () => {
     let title = this.parentNode.parentNode.parentNode.querySelectorAll('.main-card-title')
     let price = this.parentNode.parentNode.parentNode.querySelectorAll('.price span')
     let image = this.parentNode.parentNode.parentNode.querySelectorAll('.main-card-img img')
-
-    console.log(title[0].innerText);
-    console.log(price[0].innerText);
-    console.log(image[0].src);
 }
-
 for (let i = 0; i < addToCard.clientHeight; i++) {
     addToCard[i].addEventListener('click', addTo, false);
 }
